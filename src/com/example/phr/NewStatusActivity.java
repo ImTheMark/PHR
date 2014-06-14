db8a14a61c71651bf47b0de091428a94042665af
ImTheMark <mark_serapion@yahoo.com>
2014-06-14 10:55:29 +0800

    Initial Push
    UI implementations

---
+++ src/com/example/phr/NewStatusActivity.java
@@ -0,0 +1,14 @@
+package com.example.phr;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NewStatusActivity extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_new_status);
+	}
+
+}
